4ba0940757e845b729007f9c1806e4b59f2af1d2
unknown <wajdimn77@gmail.com>
2022-08-06 19:05:22 +0100

    save password in JSON file and Add the search function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from template import *
 import pyperclip
+import json
 import random
 
 
@@ -33,20 +34,42 @@ def save():
     new_website = website[1].get()  # get the entry value
     new_username = user_name[1].get()
     new_password = password[1].get()
+    new_data = {new_website: {"password": new_password, "user_name": new_username}}
     if len(new_website) > 0 and len(new_password) > 0 and len(new_username) > 0:
         is_ok = messagebox.askokcancel(title=new_website, message="Are you sure ?")
         if is_ok:
-            with open("my_passwords.txt", "a") as my_passwords:
-                my_passwords.write(f"{new_website} | {new_username} | {new_password}\n")
+            # Use try catch to create a file if we got a FileNotFoundError
+            try:
+                with open("my_passwords.json", "r") as my_passwords:
+                    data = json.load(my_passwords)
+                    data.update(new_data)
+                with open("my_passwords.json", "w") as my_passwords:
+                    json.dump(data, my_passwords, indent=2)
+            except FileNotFoundError:
+                with open("my_passwords.json", "w") as my_passwords:
+                    json.dump(new_data, my_passwords, indent=2)
+            finally:
                 website[1].delete(0, END)  # get the entry value
                 user_name[1].delete(0, END)
                 password[1].delete(0, END)
+
     else:
         messagebox.showerror(title="Oops",
                              message="Please dont leave any fields empty!")  # a popup for showing a error message
 
 
-# ---------------------------- UI SETUP ------------------------------- #
+def search():
+    with open("my_passwords.json", "r") as my_passwords:
+        data = json.load(my_passwords)
+        website_value = website[1].get()  # get the entry value
+        if website_value in data:
+            messagebox.showinfo(title=website_value,
+                                message=f"user_name : {data[website_value]['user_name']}\npassword : {data[website_value]['password']}")
+        else :
+            messagebox.showerror(title="Not found",message="This website is not in your passwords")
+        # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
@@ -61,6 +84,7 @@ website = input_template("Website :", 35, 0, 1, columnspan=2)
 user_name = input_template("Email/Username :", 35, 0, 2, columnspan=2)
 password = input_template("Password :", 21, 0, 3)
 # buttons
+search = btn_template("Search", grid_column=3, grid_row=1, command=search)
 generate = btn_template("Generate Password", grid_column=2, grid_row=3, command=password_generator)
 Add = btn_template("Add", grid_column=1, grid_row=4, btn_width=36, command=save, columnspan=2)
 window.mainloop()
